e21d9638d771b59278cc0966148bce7fe0300a1b
Anjana Jolly <anjana.j@cumulations.com>
2022-01-10 11:54:45 +0530

    url changed

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:news_app/common/theme.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +21,9 @@ class _NewScreenState extends State<NewScreen> {
   var refreshKey = GlobalKey<RefreshIndicatorState>();
   var cat = {};
   bool _isSubscribed = false;
+  var kMobileBreakpoint = 576;
+  var kTabletBreakpoint = 1024;
+  var kDesktopBreakPoint = 1366;
 
   Future<Null> _refresh() async {
     await Future.delayed(Duration(seconds: 2));
@@ -78,8 +84,8 @@ class _NewScreenState extends State<NewScreen> {
                       padding: EdgeInsets.only(right: 16, bottom: 14, top: 30),
                       // ignore: deprecated_member_use
                       child: RaisedButton(
-                        onPressed: () =>
-                            Navigator.pushNamed(context, '/joinmaillist'),
+                        onPressed: () => _launchURL(
+                            'https://manage.kmail-lists.com/subscriptions/subscribe?a=WkuMfY&g=VzKJMJ'),
                         color: Color(0xFFFC7C54),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(6),
@@ -140,10 +146,38 @@ class _NewScreenState extends State<NewScreen> {
                     );
                   } else {
                     print((snapshot.data as dom.Document).children[0]);
-                    return SingleChildScrollView(
-                        controller: _scrollController,
-                        child:
-                            (kIsWeb) ? gridView(snapshot) : listView(snapshot));
+                    int itemCount = (snapshot.data as dom.Document)
+                        .children[0]
+                        .getElementsByTagName('channel')[0]
+                        .getElementsByTagName('item')
+                        .length;
+                    return LayoutBuilder(
+                      builder: (context, dimens) {
+                        if (dimens.maxWidth <= kMobileBreakpoint) {
+                          return CustomGridView(
+                              columnRatio: 6,
+                              itemCount: itemCount,
+                              snapshot: snapshot);
+                        } else if (dimens.maxWidth > kMobileBreakpoint &&
+                            dimens.maxWidth <= kTabletBreakpoint) {
+                          return CustomGridView(
+                              columnRatio: 4,
+                              itemCount: itemCount,
+                              snapshot: snapshot);
+                        } else if (dimens.maxWidth > kTabletBreakpoint &&
+                            dimens.maxWidth <= kDesktopBreakPoint) {
+                          return CustomGridView(
+                              columnRatio: 3,
+                              itemCount: itemCount,
+                              snapshot: snapshot);
+                        } else {
+                          return CustomGridView(
+                              columnRatio: 3,
+                              itemCount: itemCount,
+                              snapshot: snapshot);
+                        }
+                      },
+                    );
                   }
                 },
               ),
@@ -154,35 +188,37 @@ class _NewScreenState extends State<NewScreen> {
     );
   }
 
-  Widget listView(AsyncSnapshot snapshot) {
-    return ListView.builder(
-        controller: _scrollController,
-        shrinkWrap: true,
-        itemCount: (snapshot.data as dom.Document)
+  Random random = new Random();
+
+  Widget CustomGridView(
+      {required int columnRatio,
+      required int itemCount,
+      required AsyncSnapshot snapshot}) {
+    return StaggeredGridView.countBuilder(
+      primary: false,
+      crossAxisCount: 12,
+      itemCount: itemCount,
+      itemBuilder: (context, index) {
+        var item = (snapshot.data as dom.Document)
             .children[0]
             .getElementsByTagName('channel')[0]
-            .getElementsByTagName('item')
-            .length,
-        itemBuilder: (context, index) {
-          var item = (snapshot.data as dom.Document)
-              .children[0]
-              .getElementsByTagName('channel')[0]
-              .getElementsByTagName('item')[index]
-              .children;
-          // print("PubDate : ${item[3].text}");
-          // print("Description : ${item[5].text}");
-          // print("Image URL : ${item[6].text}");
-          var list = (item[3].text).split(",")[1].trim().split(" ");
-          var dateString = "${list[0]} ${list[1]} ${list[2]}";
-          // print(list);
-          // var category = <String>
-          // print('item 4 ' + item[4].text);
-          // var category =
-          // var list1=list[1].trim()
-          // var date = DateTime.parse(item[3].text);
-          // print(date.toString());
-          return GestureDetector(
-            onTap: () {
+            .getElementsByTagName('item')[index]
+            .children;
+        var list = (item[3].text).split(",")[1].trim().split(" ");
+        //
+        var dateString = "${list[0]} ${list[1]} ${list[2]}";
+        return GestureDetector(
+          onTap: () {
+            if (kIsWeb)
+              _launchURL((snapshot.data as dom.Document)
+                  .children[0]
+                  .getElementsByTagName('channel')[0]
+                  .getElementsByTagName('item')[index]
+                  .text
+                  .trim()
+                  .split("\n")[1]
+                  .trim());
+            else {
               Navigator.pushNamed(context, '/webview',
                       arguments: (snapshot.data as dom.Document)
                           .children[0]
@@ -193,139 +229,91 @@ class _NewScreenState extends State<NewScreen> {
                           .split("\n")[1]
                           .trim())
                   .then((_) => setState(() {}));
-            },
-            child: Container(
-              padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-              // height: 143 * ThemesData.heightRatio,
-              // width: 335 * ThemesData.widthRatio,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                elevation: 16,
+            }
+          },
+          //
+
+          child: Container(
+            padding: EdgeInsets.all(6),
+            decoration: BoxDecoration(
                 color: ThemesData.CARD_COLOR,
-                child: Container(
-                  // height: 143 * ThemesData.heightRatio,
-                  // width: 335 * ThemesData.widthRatio,
-                  margin: EdgeInsets.all(12),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            ((item[6].text.length > 5 &&
-                                    !item[6].text.contains("\n")))
-                                ? ClipRRect(
-                                    child: CachedNetworkImage(
-                                      imageUrl: (item[6].text.length > 5 &&
-                                              !item[6].text.contains("\n"))
-                                          ? item[6]
-                                              .text
-                                              .substring(0, item[6].text.length)
-                                          : "https://news.maxabout.com/wp-content/uploads/2018/06/Quick-Facts-BMW-G310GS.jpg",
-                                      height: 88 * ThemesData.heightRatio,
-                                      width: 88 * ThemesData.widthRatio,
-                                      fit: BoxFit.fill,
-                                      placeholder: (context, value) {
-                                        return Image.asset(
-                                          "assets/placeholder.png",
-                                          fit: BoxFit.cover,
-                                          height: 88 * ThemesData.heightRatio,
-                                          width: 88 * ThemesData.widthRatio,
-                                        );
-                                      },
-                                    ),
-                                    borderRadius: BorderRadius.circular(4),
-                                  )
-                                : Container(),
-                            ((item[6].text.length > 5 &&
-                                    !item[6].text.contains("\n")))
-                                ? SizedBox(
-                                    width: 8 * ThemesData.widthRatio,
-                                  )
-                                : Container(
-                                    height: 88 * ThemesData.heightRatio,
-                                  ),
-                            Expanded(
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(
-                                    item[0].text,
-                                    // "abc",
-                                    style: TextStyle(
-                                        fontFamily: 'GilroyRegular',
-                                        color: const Color(0xffD9E2FF),
-                                        fontSize: 16 * ThemesData.widthRatio,
-                                        fontWeight: FontWeight.bold),
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  SizedBox(
-                                    height: 14 * ThemesData.heightRatio,
-                                  ),
-                                  Text(
-                                    item[5].text,
-                                    // "aaa",
-                                    style: TextStyle(
-                                        fontFamily: "GilroyRegular",
-                                        fontSize: 14 * ThemesData.heightRatio,
-                                        fontWeight: FontWeight.w500,
-                                        color: const Color(0xFFD9E2FF)),
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
+                borderRadius: BorderRadius.circular(4),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0, 2),
+                      blurRadius: 6)
+                ]),
+            height: random.nextInt(75).toDouble() + 300,
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  !((item[6].text.length > 5 && !item[6].text.contains("\n")))
+                      ? Container(
+                          height: 0,
+                          width: 0,
+                        )
+                      : ClipRRect(
+                          child: CachedNetworkImage(
+                            imageUrl: (item[6].text.length > 5 &&
+                                    !item[6].text.contains("\n"))
+                                ? item[6].text.substring(0, item[6].text.length)
+                                : "https://news.maxabout.com/wp-content/uploads/2018/06/Quick-Facts-BMW-G310GS.jpg",
+                            height: 150,
+                            width: 150,
+                            fit: BoxFit.fill,
+                            placeholder: (context, value) {
+                              return Image.asset(
+                                "assets/placeholder.png",
+                                fit: BoxFit.cover,
+                                height: 88 * ThemesData.heightRatio,
+                                width: 88 * ThemesData.widthRatio,
+                              );
+                            },
+                          ),
+                          borderRadius: BorderRadius.circular(4),
                         ),
-                      ),
-                      SizedBox(
-                        height: 8 * ThemesData.heightRatio,
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            item[4].text,
-                            style: TextStyle(
-                                color: const Color.fromRGBO(217, 226, 255, 0.6),
-                                fontSize: 12 * ThemesData.widthRatio,
-                                fontFamily: "GilroyRegular"),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 8, right: 8),
-                            height: 4 * ThemesData.heightRatio,
-                            width: 4 * ThemesData.widthRatio,
-                            decoration: BoxDecoration(
-                                color: const Color.fromRGBO(217, 226, 255, 0.6),
-                                shape: BoxShape.circle),
-                          ),
-                          Text(
-                            // DateFormat("dd MMM yyyy")
-                            //     .format(DateTime.parse(
-                            //         "2012-02-27")),
-                            dateString,
-                            style: TextStyle(
-                                color: const Color.fromRGBO(217, 226, 255, 0.6),
-                                fontSize: 12 * ThemesData.widthRatio,
-                                fontFamily: "GilroyRegular"),
-                          ),
-                        ],
-                      )
-                    ],
+                  SizedBox(
+                    height: 15,
                   ),
-                ),
-              ),
-            ),
-          );
-        });
+                  Container(
+                    child: Text(
+                      item[0].text,
+                      // "abc",
+                      style: TextStyle(
+                          fontFamily: 'GilroyRegular',
+                          color: const Color(0xffD9E2FF),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    item[5].text,
+                    // "aaa",
+                    style: TextStyle(
+                        fontFamily: "GilroyRegular",
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0xFFD9E2FF)),
+                    maxLines: ((item[6].text.length > 5 &&
+                            !item[6].text.contains("\n")))
+                        ? 2
+                        : 6,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ]),
+          ),
+        );
+      },
+      staggeredTileBuilder: (index) => StaggeredTile.fit(columnRatio),
+    );
   }
 
   Widget gridView(AsyncSnapshot snapshot) {
@@ -425,7 +413,7 @@ class _NewScreenState extends State<NewScreen> {
                       color: const Color(0xffD9E2FF),
                       fontSize: 20,
                       fontWeight: FontWeight.bold),
-                  maxLines: 2,
+                  maxLines: 3,
                   overflow: TextOverflow.ellipsis,
                 ),
                 SizedBox(
@@ -436,7 +424,7 @@ class _NewScreenState extends State<NewScreen> {
                   // "aaa",
                   style: TextStyle(
                       fontFamily: "GilroyRegular",
-                      fontSize: 14 * ThemesData.heightRatio,
+                      fontSize: 16,
                       fontWeight: FontWeight.w500,
                       color: const Color(0xFFD9E2FF)),
                   maxLines: 5,
@@ -451,7 +439,7 @@ class _NewScreenState extends State<NewScreen> {
                 item[4].text,
                 style: TextStyle(
                     color: const Color.fromRGBO(217, 226, 255, 0.6),
-                    fontSize: 12,
+                    fontSize: 14,
                     fontFamily: "GilroyRegular"),
               ),
               Container(
